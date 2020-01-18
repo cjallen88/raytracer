9f9dce7f0ce3f79c18c034c1cad35018f831d4cc
Craig Allen <craig.allen@virtualtrader.com>
2020-01-18 11:11:52 +0000

    add diffuse shading + lights

--- raytracer.lisp
+++ raytracer.lisp
@@ -5,11 +5,10 @@
                               :blue (make-vec :x 0.0 :y 0.0 :z 1.0)
                               :green (make-vec :x 0.0 :y 1.0 :z 0.0)
                               :black (make-vec :x 0.0 :y 0.0 :z 0.0)
-                              :white (make-vec :x 1.0 :y 1.0 :z 1.0)))
+                              :white (make-vec :x 1.0 :y 1.0 :z 1.0)
+                              :orange (make-vec :x 1.0 :y 0.5 :z 0.0)))
 
-(defparameter *background-colour* (getf *colours* :white))
-
-(defparameter *view-distance* 1000)
+(defparameter *background-colour* (getf *colours* :black))
 
 (defclass sphere ()
   ((centre :initarg :centre :accessor centre)
@@ -21,8 +20,20 @@
 
 (defmethod print-object ((obj sphere) stream)
   (print-unreadable-object (obj stream :type t)
-    (with-slots (centre radius) obj
-      (format stream "centre: ~a, radius: ~a" centre radius))))
+    (with-slots (centre radius colour) obj
+      (format stream "centre: ~a, radius: ~a, colour: ~a" centre radius colour))))
+
+(defclass point-light ()
+  ((pos :initarg :pos :accessor pos)
+   (intensity :initarg :intensity :accessor intensity :type float)))
+
+(defun make-point-light (&key pos (intensity 1.0))
+  (make-instance 'point-light :pos pos :intensity intensity))
+
+(defmethod print-object ((obj point-light) stream)
+  (print-unreadable-object (obj stream :type t)
+    (with-slots (pos intensity) obj
+      (format stream "position: ~a, intensity: ~a" pos intensity))))
 
 (defclass ray ()
   ((origin :initarg :origin :accessor origin)
@@ -53,43 +64,60 @@ https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rend
                 l)) ; is origin inside sphere
             ;; else centre of sphere projects on the array
             (let ((radius-sqrd (expt radius 2)))
-              (if (> d-sqrd radius-sqrd)
-                  nil
-                  (let* ((thc (sqrt (- radius-sqrd d-sqrd)))
-                         (t0 (- tca thc))  ; intersect front
-                         (t1 (+ tca thc))) ; intersect back
-                    (when (> t0 t1) (rotatef t0 t1))
-                    (when (> t0 0)
-                      t0)))))))))
+              (unless (> d-sqrd radius-sqrd)
+                (let* ((thc (sqrt (- radius-sqrd d-sqrd)))
+                       (t0 (- tca thc))  ; intersect front
+                       (t1 (+ tca thc))) ; intersect back
+                  (when (> t0 t1) (rotatef t0 t1))
+                  (when (> t0 0)
+                    t0)))))))))
 
-(defun cast-ray (ray scene)
+(defun diffuse-intensity (ray hit-object hit-distance lights)
+  "Returns the colour of the hitpoint modified by light intensity and surface normal
+https://github.com/ssloy/tinyraytracer/commit/9a728fff2bbebb1eedd86e1ac89f657d43191609
+https://www.youtube.com/watch?v=5apJJKd4z-s"
+  (let* ((hit-point (v-add (origin ray)
+                           (v-mul (direction ray) hit-distance)))
+         (hit-normal (v-normalize (v-sub hit-point
+                                         (centre hit-object)))))
+    (flet ((light-intensity-reducer (acc l)
+             (let ((light-dir (v-normalize (v-sub (pos l) hit-point))))
+               (+ acc (* (intensity l)
+                         (max 0.0
+                              (min 1.0 (v-dot hit-normal light-dir))))))))
+      (reduce #'light-intensity-reducer
+              lights
+              :initial-value 0.0))))
+
+(defun cast-ray (ray scene lights)
+  "Returns the colour of the ray depending on whether it hits anything"
   (let* ((hits (mapcar (lambda (obj)
                          (list obj (intersect ray obj)))
                        scene))
          (closest
            (iter (for h in hits)
              (when (second h)
-               (finding (first h) minimizing (second h))))))
+               (finding h minimizing (second h))))))
     (if closest
-        (colour closest)
-        *background-colour*)))  
-
-(defun deg-to-rad (degrees) (* pi (/ degrees 180.0)))
-(defun rad-to-deg (radians) (/ (* radians 180.0) pi)) 
+        (v-mul (colour (first closest))
+               (diffuse-intensity ray (first closest) (second closest) lights))
+        *background-colour*)))
 
 (defun raster-to-camera-coord (axis-pos axis-scale x-or-y)
   "https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-generating-camera-rays/generating-camera-rays"
-  (let* ((ndc ; Normalized Device Coordinates, mapping raster space to world space [0,1]
-           (/ (+ axis-pos 0.5) ; 0.5 to push position to centre of pixel
-              (float axis-scale)))
-         (screenspace-coord ; convert x ndc to screenspace [-1,1]
-           (ecase x-or-y
-             (:x (- (* 2 ndc) 1))
-             (:y (- 1 (* 2 ndc)))))
-         (fov-angle 90)
-         (scale (tan (deg-to-rad (* fov-angle 0.5))))
-         (coord (* screenspace-coord scale)))
-    coord))
+  (flet ((deg-to-rad (degrees) (* pi (/ degrees 180.0)))
+         (rad-to-deg (radians) (/ (* radians 180.0) pi)))
+    (let* ((ndc ; Normalized Device Coordinates, mapping raster space to world space [0,1]
+             (/ (+ axis-pos 0.5) ; 0.5 to push position to centre of pixel
+                (float axis-scale)))
+           (screenspace-coord ; convert x ndc to screenspace [-1,1]
+             (ecase x-or-y
+               (:x (- (* 2 ndc) 1))
+               (:y (- 1 (* 2 ndc)))))
+           (fov-angle 90)
+           (scale (tan (deg-to-rad (* fov-angle 0.5))))
+           (coord (* screenspace-coord scale)))
+      coord)))
 
 (defun print-colour (s vec)
   "Calculate colour from given vector, clamps max values to 255
@@ -106,7 +134,7 @@ to prevent issues when diffuse light intensity is too high"
     (iter (for i from 0 to (1- (* w h)))  
       (print-colour s (aref fb i)))))
 
-(defun render (filename width height scene)
+(defun render (filename width height scene lights)
   (let ((fb (make-array (* width height))))
     (iter (for j from 0 to (1- height))
       (iter (for i from 0 to (1- width))
@@ -118,5 +146,5 @@ to prevent issues when diffuse light intensity is too high"
                (dir (v-normalize (make-vec :x x :y y :z -1.0)))
                (ray (make-ray :origin camera :direction dir)))
           (setf (aref fb (+ i (* j width)))
-                (cast-ray ray scene)))))
+                (cast-ray ray scene lights)))))
     (print-fb filename width height fb)))
--- test.lisp
+++ test.lisp
@@ -18,6 +18,14 @@
         
         (raytracer:make-sphere :centre (vector:make-vec :x 0.2 :y 0.2 :z -7.0)
                                :radius 1.4
-                               :colour (getf raytracer:*colours* :red))))
+                               :colour (getf raytracer:*colours* :red))
+        
+        (raytracer:make-sphere :centre (vector:make-vec :x -0.0 :y -14.0 :z -9.0)
+                               :radius 12.0
+                               :colour (getf raytracer:*colours* :orange))))
 
-(raytracer:render "scene-test.ppm" 100 100 *scene*)
+(defparameter *lights*
+  (list (raytracer::make-point-light :pos (vector:make-vec :x 3.0 :y -2.0 :z -1.0) :intensity 0.9)
+        (raytracer::make-point-light :pos (vector:make-vec :x -2.5 :y -2.0 :z -6.0) :intensity 0.7)))
+
+(raytracer:render "scene-test.ppm" 500 500 *scene* *lights*)
